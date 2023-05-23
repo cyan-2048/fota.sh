12d5774ccdd69e4495133878106bb013b79dd51a
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2023-05-23 10:46:52 +0800

    oopsie

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
+// @ts-ignore
+const production = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	base: production ? "/fota.sh/" : "/",
 	server: {
 		port: 3000,
 	},
